200bea33ac8090f7b3a82dcbd26805daaeef6f4d
KX-Apple <57068530+AnAppleforlife@users.noreply.github.com>
2021-04-27 21:58:18 +0200

    🐛Fixes

--- backend/videoServing/stars.ts
+++ backend/videoServing/stars.ts
@@ -10,7 +10,7 @@ function setStars(token: string, ip:string, path:string, stars:number) : BasicAn
 
     if (!user.status) return user
 
-    if (stars < 0 || stars > 5 || !Number.isInteger(stars))
+    if (!Number.isInteger(stars) || stars < 0 || stars > 5)
         return { status: false, reason: 'The stars has to be an integer between 0 and 5' }
 
     const checkedPath = checkPath(path);
